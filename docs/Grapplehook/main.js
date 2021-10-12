88837af80972fda4b3f814846663e2ed8e463e1e
Evan Li <eli24@ucsc.edu>
2021-10-11 17:18:38 -0700

    gravity and player launching

--- docs/Grapplehook/main.js
+++ docs/Grapplehook/main.js
@@ -1,6 +1,7 @@
-title = "";
+title = "Grapplehook";
 
 description = `
+Click to shoot a grapplehook
 `;
 
 characters = [];
@@ -23,7 +24,19 @@ options = {
 * @typedef { object } Player
 * @property { Color } color
 * @property { Vector } pos
+* @property { Vector } velocity
+* @property { Vector } gravity
+* @property { Number } launchSpeed
+* @property { Number } grappleSlowdown
+*/
+
+/**
+* @typedef { object } Grapple
+* @property { Color } color
+* @property { Vector } pos
+* @property { Vector } velocity
 * @property { Number } speed
+
 */
 
 /**
@@ -31,10 +44,17 @@ options = {
 */
 let player
 
+/**
+* @type  { Grapple }
+*/
+let grapple
+
 function update() {
     if (!ticks) {
         Start();
     }
+
+    PlayerInput();
 
     RenderPlayer();
 }
@@ -44,12 +64,60 @@ function Start()
     player = {
         color: "cyan",
         pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
+        velocity: vec(0, 0),
+        gravity: vec(0, 0.1),
+        launchSpeed: 5,
+        grappleSlowdown: 0.5,
+    };
+
+    grapple = {
+        color: "green",
+        pos: vec(G.WIDTH * 0.5, G.HEIGHT * 0.5),
+        velocity: vec(0, 0),
         speed: 5,
     };
 }
 
 function RenderPlayer()
 {
+    
+    player.velocity.add(player.gravity);
+
+    player.pos.add(player.velocity);
+    player.pos.wrap(0, G.WIDTH, 0, G.HEIGHT);
     color(player.color);
     box(player.pos.x, player.pos.y, 10);
+}
+
+function PlayerInput()
+{
+    if(input.isJustPressed)
+    {
+        let currInput = vec(input.pos.x, input.pos.y);
+        let inputVector = vec(currInput.x - player.pos.x,
+        currInput.y - player.pos.y);
+        let inputDirection = inputVector.normalize();
+        
+        ShootGrapple(inputDirection, inputVector.length);
+    }
+}
+
+function ShootGrapple(inputDirection, inputLength)
+{
+    LaunchPlayer(inputDirection, inputLength);
+}
+
+function LaunchPlayer(launchDirection, launchLength)
+{
+    let launchVelocity = RescaleVector(launchDirection, 
+        launchLength * player.launchSpeed);
+
+    player.velocity = RescaleVector(player.velocity, player.grappleSlowdown)
+
+    player.velocity.add(launchVelocity);
+}
+
+function RescaleVector(toScale, multiplier)
+{
+    return vec(toScale.x * multiplier, toScale.y * multiplier);
 }
